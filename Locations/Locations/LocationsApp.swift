15eb822eccdce5be9ff50f60ee153b5879614575
Nicolas Alejandro Fernandez Amorosino <nicolasfernandez93@gmail.com>
2025-05-20 16:50:22 -0300

    Added tests to all searchable data sets

--- Locations/Locations/LocationsApp.swift
+++ Locations/Locations/LocationsApp.swift
@@ -13,43 +13,8 @@ struct LocationsApp: App {
 
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-            TestView()
-        }
-    }
-}
-
-
-struct TestView: View {
-    let jsonReader = JSONReader()
-
-    var body: some View {
-        Text("Hello world!")
-            .onAppear {
-                loadDataSet()
-            }
-    }
-
-    func loadDataSet() {
-        let trie = Trie()
-        let arrayDataSet = ArrayDataSet()
-        let ternarySearchTree = TernarySearchTree()
-        let benchmarker = Benchmarker(
-            searchableDataSets: [
-                trie,
-                arrayDataSet,
-                ternarySearchTree
-            ],
-            citiesRepository: LocalCityRepository()
-        )
-
-        Task {
-            do {
-                try await benchmarker.startBenchmark(prefix: "Basconcillos del Tozo", iterations: 10000)
-            } catch {
-                print(error.localizedDescription)
-            }
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
---
+++ Locations/LocationsTests/TernarySearchTreeTests.swift
@@ -0,0 +1,97 @@
+//
+//  TernarySearchTreeTests.swift
+//  LocationsTests
+//
+//  Created by Nicolas Alejandro Fernandez Amorosino on 20/05/2025.
+//
+
+import XCTest
+@testable import Locations
+
+final class TernarySearchTreeTests: XCTestCase {
+    var sut: TernarySearchTree!
+
+    override func setUpWithError() throws {
+        sut = TernarySearchTree()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_A_returnsFourElements() throws {
+        // Given
+        let cities = mockCities()
+        let prefix = "A"
+
+        cities.forEach { sut.insert($0) }
+
+        // When
+        let result = sut.search(prefix: prefix)
+
+        // Then
+        XCTAssertEqual(result.count, 4)
+        XCTAssertTrue(cities.contains(where: { $0.name == "Alabama" } ))
+        XCTAssertTrue(cities.contains(where: { $0.name == "Albuquerque" } ))
+        XCTAssertTrue(cities.contains(where: { $0.name == "Anaheim" } ))
+        XCTAssertTrue(cities.contains(where: { $0.name == "Arizona" } ))
+    }
+
+    func test_s_returnsOneElement() throws {
+        // Given
+        let cities = mockCities()
+        let prefix = "s"
+
+        cities.forEach { sut.insert($0) }
+
+        // When
+        let result = sut.search(prefix: prefix)
+
+        // Then
+        XCTAssertEqual(result.count, 1)
+        XCTAssertTrue(cities.contains(where: { $0.name == "Sydney" } ))
+    }
+
+    func test_Al_returnsTwoElements() throws {
+        // Given
+        let cities = mockCities()
+        let prefix = "Al"
+
+        cities.forEach { sut.insert($0) }
+
+        // When
+        let result = sut.search(prefix: prefix)
+
+        // Then
+        XCTAssertEqual(result.count, 2)
+        XCTAssertTrue(cities.contains(where: { $0.name == "Alabama" } ))
+        XCTAssertTrue(cities.contains(where: { $0.name == "Albuquerque" } ))
+    }
+
+    func test_Alb_returnsOneElement() throws {
+        // Given
+        let cities = mockCities()
+        let prefix = "Alb"
+
+        cities.forEach { sut.insert($0) }
+
+        // When
+        let result = sut.search(prefix: prefix)
+
+        // Then
+        XCTAssertEqual(result.count, 1)
+        XCTAssertTrue(cities.contains(where: { $0.name == "Albuquerque" } ))
+    }
+}
+
+extension XCTestCase {
+    func mockCities() -> [City] {
+        [
+            City(id: 0, country: "US", name: "Alabama", coordinate: Coordinate(longitude: 0.0, latitude: 0.0)),
+            City(id: 1, country: "US", name: "Albuquerque", coordinate: Coordinate(longitude: 0.0, latitude: 0.0)),
+            City(id: 2, country: "US", name: "Anaheim", coordinate: Coordinate(longitude: 0.0, latitude: 0.0)),
+            City(id: 3, country: "US", name: "Arizona", coordinate: Coordinate(longitude: 0.0, latitude: 0.0)),
+            City(id: 4, country: "AU", name: "Sydney", coordinate: Coordinate(longitude: 0.0, latitude: 0.0))
+        ]
+    }
+}
